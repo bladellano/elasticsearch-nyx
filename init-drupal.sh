a08a25cea47781c70b3da1a92e676735ed66b42f
Caio Dellano <bladellano@gmail.com>
2025-06-17 09:33:03 +0100

    Adicionar o módulo ImageMagick e atualizar o composer.lock

--- init-drupal.sh
+++ init-drupal.sh
@@ -5,7 +5,7 @@ cd /var/www/html
 
 # Executa os comandos do Drush
 ./vendor/bin/drush sql-drop -y
-./vendor/bin/drush sqlc < ./database/dump_2025-06-03_183758.sql
+./vendor/bin/drush sqlc < ./database/dump_2025-06-17_083226.sql
 ./vendor/bin/drush updb -y
 ./vendor/bin/drush cim -y
 ./vendor/bin/drush cr
